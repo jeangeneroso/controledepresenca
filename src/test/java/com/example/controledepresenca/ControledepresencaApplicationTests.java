0b8b021d7d7d008293c4002c9d1d9d7023e8bd16
Jean Generoso <jean.generoso@yahoo.com.br>
2026-03-08 17:19:02 -0300

    Ajustes no codigo

---
+++ src/test/java/com/example/controledepresenca/ControledepresencaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.controledepresenca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControledepresencaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
