158a233e1e4023822974d6db4ce2f8a8b7fda58b
Jean Generoso <jean.generoso@yahoo.com.br>
2026-05-31 17:14:19 -0300

    Ajustes na classe ColaboradorController

--- src/main/java/com/example/controledepresenca/controller/LiderController.java
+++ src/main/java/com/example/controledepresenca/controller/LiderController.java
@@ -2,10 +2,16 @@ package com.example.controledepresenca.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.controledepresenca.model.Colaborador;
 import com.example.controledepresenca.model.Lider;
 import com.example.controledepresenca.service.ColaboradorService;
 import com.example.controledepresenca.service.LiderService;
@@ -26,6 +32,27 @@ public class LiderController {
 		return liderService.listarTodos();
 	}
 	
+	@GetMapping("/{id}")
+	public Lider getPorId (@PathVariable Integer id) {
+		return liderService.listTodosPorId(id);
+	}
+	
+	
+	@PostMapping
+	public Lider criar (@RequestBody Lider lider) {
+		return liderService.salvar(lider);	
+	}
 	
 
+	@PutMapping("/{id}")
+	public Lider atualizar (@PathVariable Integer id, @RequestBody Lider lider) {
+		return liderService.atualizar(id, lider);		
+	}
+	
+	
+	@DeleteMapping("/{id}")
+    public void excluir(@PathVariable Integer id) {
+        liderService.excluir(id);
+    }
+	
 }
--- src/main/java/com/example/controledepresenca/service/LiderService.java
+++ src/main/java/com/example/controledepresenca/service/LiderService.java
@@ -11,9 +11,26 @@ public class LiderService {
 		return repository.findAll();
 	}
 
-	public Lider listTodosPorId() {
+	public Lider listTodosPorId(Integer id) {
 		// TODO Auto-generated method stub
-		return repository.findAll();
+		return null;
 	}
 
+
+	public Lider salvar(Lider lider) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Lider atualizar(Integer id, Lider lider) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void excluir(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
 }
