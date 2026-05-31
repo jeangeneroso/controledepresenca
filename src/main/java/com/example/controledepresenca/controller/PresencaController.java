7557b6efcca8d5515337ecb768f86c8ea5b5d369
Jean Generoso <jean.generoso@yahoo.com.br>
2026-05-31 17:49:10 -0300

    Ajustes na classe PresencaController

--- src/main/java/com/example/controledepresenca/controller/PresencaController.java
+++ src/main/java/com/example/controledepresenca/controller/PresencaController.java
@@ -15,19 +15,33 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.controledepresenca.model.Colaborador;
 import com.example.controledepresenca.model.Presenca;
 import com.example.controledepresenca.service.ColaboradorService;
+import com.example.controledepresenca.service.PresencaService;
 
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
+@RestController
+@RequestMapping("/presencas")
 public class PresencaController {
+	
+	private final PresencaService presencaService;
+	
+	public PresencaController (PresencaService presencaService) {
+		this.presencaService = presencaService;
+	}
+
+	 @GetMapping
+	    public List<Presenca> getTodos() {
+	        return presencaService.listarTodos();
+	    }
 	
     @PostMapping("/{id}/presenca")
     public Presenca incluirPresencaColabodor(@PathVariable Integer id, @RequestBody Date dataPresenca) {
-        return presencaService.incluirPresenca(id, dataPresenca);
+        return presencaService.incluirPresencaColaborador(id, dataPresenca);
     }
     
     @PostMapping("/{id}/presenca")
     public Presenca incluirPresencaLider(@PathVariable Integer id, @RequestBody Date dataPresenca) {
-        return presencaService.incluirPresenca(id, dataPresenca);
+        return presencaService.incluirPresencaLider(id, dataPresenca);
     }
 
 
--- src/main/java/com/example/controledepresenca/service/PresencaService.java
+++ src/main/java/com/example/controledepresenca/service/PresencaService.java
@@ -1,5 +1,25 @@
 package com.example.controledepresenca.service;
 
+import java.util.Date;
+import java.util.List;
+
+import com.example.controledepresenca.model.Presenca;
+
 public class PresencaService {
+
+	public List<Presenca> listarTodos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Presenca incluirPresencaColaborador(Integer id, Date dataPresenca) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Presenca incluirPresencaLider(Integer id, Date dataPresenca) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
