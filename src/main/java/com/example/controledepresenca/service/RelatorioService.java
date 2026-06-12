5e99248655b87a97c98f4609e66c4ccc5bd13f4c
Jean Generoso <jean.generoso@yahoo.com.br>
2026-06-12 17:54:04 -0300

    Ajustes no pacote service

--- src/main/java/com/example/controledepresenca/service/RelatorioService.java
+++ src/main/java/com/example/controledepresenca/service/RelatorioService.java
@@ -1,5 +1,33 @@
 package com.example.controledepresenca.service;
 
+import com.example.controledepresenca.model.Colaborador;
+import com.example.controledepresenca.model.Lider;
+import com.example.controledepresenca.model.Relatorio;
+import com.example.controledepresenca.repository.RelatorioRepository;
+
+import java.util.List;
+
 public class RelatorioService {
 
+    private final RelatorioRepository repository;
+
+    public RelatorioService(RelatorioRepository repository) {
+        this.repository = repository;
+    }
+
+   public List<Relatorio> listarColaborador() {
+        return repository.findAll();
+    }
+
+    public List<Relatorio> listarLider() {
+        return repository.findAll();
+    }
+
+    public List<Colaborador> getColaborador() {
+        return List.of();
+    }
+
+    public List<Lider> getLider() {
+        return List.of();
+    }
 }
