1d263284d3d38d04577514931fa635c47073c725
Jean Generoso <jean.generoso@yahoo.com.br>
2026-06-12 17:53:38 -0300

    Ajustes no pacote controller

--- src/main/java/com/example/controledepresenca/controller/RelatorioController.java
+++ src/main/java/com/example/controledepresenca/controller/RelatorioController.java
@@ -1,5 +1,29 @@
 package com.example.controledepresenca.controller;
 
+import com.example.controledepresenca.model.Colaborador;
+import com.example.controledepresenca.model.Lider;
+import com.example.controledepresenca.service.RelatorioService;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
 public class RelatorioController {
+
+    private final RelatorioService relatorioService;
+
+    public RelatorioController(RelatorioService relatorioService) {
+        this.relatorioService = relatorioService;
+    }
+
+    @GetMapping("/colaborador")
+    public List<Colaborador> getColaborador () {
+        return relatorioService.getColaborador();
+    }
+
+    @GetMapping("/lider")
+    public List<Lider> getLider () {
+        return relatorioService.getLider();
+    }
+
 
 }
