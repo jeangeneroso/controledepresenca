ed0e3e4b5bc542ed9ab7b77d02feb65e22a93ac6
Jean Generoso <jean.generoso@yahoo.com.br>
2026-07-01 18:23:48 -0300

    Ajustes no codigo fonte

--- src/main/java/com/example/controledepresenca/controller/LiderController.java
+++ src/main/java/com/example/controledepresenca/controller/LiderController.java
@@ -3,6 +3,8 @@ package com.example.controledepresenca.controller;
 import java.util.List;
 
 import com.example.controledepresenca.dto.LiderCadastroDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -38,8 +40,8 @@ public class LiderController {
 	
 	
 	@PostMapping
-	public Lider criar (@RequestBody LiderCadastroDTO dto) {
-		return liderService.salvar(dto);
+	public ResponseEntity<Lider> criar (@RequestBody LiderCadastroDTO dto) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(liderService.salvar(dto));
 	}
 	
 
