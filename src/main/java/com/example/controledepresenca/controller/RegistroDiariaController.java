92209d82532d3209d29cb4152ea403e06272f9b0
Jean Generoso <jean.generoso@yahoo.com.br>
2026-07-04 16:59:03 -0300

    Ajustes no codigo fonte no pacote controller

--- src/main/java/com/example/controledepresenca/controller/RegistroDiariaController.java
+++ src/main/java/com/example/controledepresenca/controller/RegistroDiariaController.java
@@ -1,46 +1,41 @@
 package com.example.controledepresenca.controller;
 
-import org.springframework.web.bind.annotation.PathVariable;
+import com.example.controledepresenca.dto.RegistroDiariaColaboradorDTO;
+import com.example.controledepresenca.dto.RegistroDiariaLiderDTO;
+import com.example.controledepresenca.model.RegistroDiariaColaborador;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.controledepresenca.model.RegistroDiaria;
+import com.example.controledepresenca.model.RegistroDiariaLider;
 import com.example.controledepresenca.service.RegistroDiariaService;
 
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
-@RequestMapping("/presencas")
+@RequestMapping("/api/presencas")
+@CrossOrigin(origins = "*")
 public class RegistroDiariaController {
 	
 	private final RegistroDiariaService presencaService;
 	
-	public RegistroDiariaController (RegistroDiariaService presencaService) {
+	public RegistroDiariaController(RegistroDiariaService presencaService) {
 		this.presencaService = presencaService;
 	}
 
     // Registrar presença de colaborador
     @PostMapping("/colaborador")
-    public RegistroDiaria incluirPresencaColaborador(
-            @RequestBody RegistroDiaria presenca) {
-        return presencaService.incluirPresencaColaborador(presenca);
+    public RegistroDiariaColaborador incluirPresencaColaborador(
+            @RequestBody RegistroDiariaColaboradorDTO dto) {
+        return presencaService.incluirPresencaColaborador(dto);
     }
+
 
     // Registrar presença de líder
-    /*@PostMapping("/lider")
-    public RegistroDiaria incluirPresencaLider(
-            @RequestBody RegistroDiaria presenca) {
-        return presencaService.incluirPresencaLider(presenca);
-    }
-*/
-
     @PostMapping("/lider")
-    public RegistroDiaria incluirPresencaLider(@RequestBody RegistroDiaria presenca) {
-        // Esse print vai te mostrar no console do IntelliJ exatamente o que o Java recebeu
-        System.out.println("Objeto recebido: " + presenca);
-        System.out.println("Líder dentro do objeto: " + presenca.getLider());
-
-        return presencaService.incluirPresencaLider(presenca);
+    public RegistroDiariaLider incluirPresencaLider(
+            @RequestBody RegistroDiariaLiderDTO dto) {
+        return presencaService.incluirPresencaLider(dto);
     }
 }
