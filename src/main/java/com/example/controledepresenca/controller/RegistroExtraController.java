f8c8443fb9ee86a1d37ba4b83e113b394e2f9e2d
Jean Generoso <jean.generoso@yahoo.com.br>
2026-07-14 10:46:07 -0300

    Ajustes na classe: RegistroExtraController.java e RegistroExtraService.java

--- src/main/java/com/example/controledepresenca/controller/RegistroExtraController.java
+++ src/main/java/com/example/controledepresenca/controller/RegistroExtraController.java
@@ -1,4 +1,38 @@
 package com.example.controledepresenca.controller;
 
+import com.example.controledepresenca.dto.RegistroExtraColaboradorDTO;
+import com.example.controledepresenca.dto.RegistroExtraLiderDTO;
+import com.example.controledepresenca.service.RegistroExtraService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/extras")
+@CrossOrigin(origins = "*")
 public class RegistroExtraController {
+
+    private final RegistroExtraService extraService;
+
+    public RegistroExtraController (RegistroExtraService extraService){
+        this.extraService = extraService;
+    }
+
+    @PostMapping("/colaborador")
+    public ResponseEntity<RegistroExtraColaboradorDTO> incluirExtraColaborador(
+            @RequestBody RegistroExtraColaboradorDTO dto) {
+
+        extraService.incluirExtraColaborador(dto);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(dto);
+    }
+
+    @PostMapping("/lider")
+    public ResponseEntity<RegistroExtraLiderDTO> incluirExtraLider(
+            @RequestBody RegistroExtraLiderDTO dto) {
+
+        extraService.incluirExtraLider(dto);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(dto);
+    }
 }
--- src/main/java/com/example/controledepresenca/service/RegistroExtraService.java
+++ src/main/java/com/example/controledepresenca/service/RegistroExtraService.java
@@ -1,4 +1,13 @@
 package com.example.controledepresenca.service;
 
+import com.example.controledepresenca.dto.RegistroExtraColaboradorDTO;
+import com.example.controledepresenca.dto.RegistroExtraLiderDTO;
+
 public class RegistroExtraService {
+
+    public void incluirExtraColaborador(RegistroExtraColaboradorDTO dto) {
+    }
+
+    public void incluirExtraLider(RegistroExtraLiderDTO dto) {
+    }
 }
