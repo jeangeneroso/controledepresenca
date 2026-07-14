c17726ed2b4b1428a9b250b9a6b6094b64d9150d
Jean Generoso <jean.generoso@yahoo.com.br>
2026-07-14 10:18:49 -0300

    Ajustes na classe: RegistroExtraColaborador.java e RegistroExtraLider.java

---
+++ src/main/java/com/example/controledepresenca/model/RegistroExtraColaborador.java
@@ -0,0 +1,74 @@
+package com.example.controledepresenca.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "extraColaborador")
+public class RegistroExtraColaborador {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate data;
+
+    @ManyToOne
+    @JoinColumn(name = "operacao_id")
+    private Operacao operacao;
+
+    @ManyToOne
+    @JoinColumn(name = "colaborador_id")
+    private Colaborador colaborador;
+
+    @JoinColumn(name = "hora")
+    private Integer quantidadeHoras;
+
+    public RegistroExtraColaborador (){
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public Operacao getOperacao() {
+        return operacao;
+    }
+
+    public void setOperacao(Operacao operacao) {
+        this.operacao = operacao;
+    }
+
+    public Colaborador getColaborador() {
+        return colaborador;
+    }
+
+    public void setColaborador(Colaborador colaborador) {
+        this.colaborador = colaborador;
+    }
+
+    public Integer getQuantidadeHoras() {
+        return quantidadeHoras;
+    }
+
+    public void setQuantidadeHoras(Integer quantidadeHoras) {
+        this.quantidadeHoras = quantidadeHoras;
+    }
+
+}
---
+++ src/main/java/com/example/controledepresenca/model/RegistroExtraLider.java
@@ -0,0 +1,69 @@
+package com.example.controledepresenca.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "extraLider")
+public class RegistroExtraLider {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate data;
+
+    @ManyToOne
+    @JoinColumn(name = "operacao_id")
+    private Operacao operacao;
+
+    @ManyToOne
+    @JoinColumn(name = "lider_id")
+    private Lider lider;
+
+    @JoinColumn(name = "hora")
+    private Integer quantidadeHoras;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public Operacao getOperacao() {
+        return operacao;
+    }
+
+    public void setOperacao(Operacao operacao) {
+        this.operacao = operacao;
+    }
+
+    public Lider getLider() {
+        return lider;
+    }
+
+    public void setLider(Lider lider) {
+        this.lider = lider;
+    }
+
+    public Integer getQuantidadeHoras() {
+        return quantidadeHoras;
+    }
+
+    public void setQuantidadeHoras(Integer quantidadeHoras) {
+        this.quantidadeHoras = quantidadeHoras;
+    }
+}
