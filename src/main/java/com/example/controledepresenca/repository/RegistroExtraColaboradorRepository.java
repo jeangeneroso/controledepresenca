64d6bcc42c50a6ab058b48b0e5e9e59d912912f8
Jean Generoso <jean.generoso@yahoo.com.br>
2026-07-14 11:13:48 -0300

    Ajustes nas classes: RegistroExtraColaboradorRepository.java e RegistroExtraLiderRepository.java

--- src/main/java/com/example/controledepresenca/repository/RegistroExtraColaboradorRepository.java
+++ src/main/java/com/example/controledepresenca/repository/RegistroExtraColaboradorRepository.java
@@ -1,4 +1,11 @@
 package com.example.controledepresenca.repository;
 
-public interface RegistroExtraColaboradorRepository {
+import com.example.controledepresenca.model.RegistroExtraColaborador;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RegistroExtraColaboradorRepository extends JpaRepository<RegistroExtraColaborador, Integer> {
+
+    RegistroExtraColaborador save(RegistroExtraColaborador extra);
 }
--- src/main/java/com/example/controledepresenca/repository/RegistroExtraLiderRepository.java
+++ src/main/java/com/example/controledepresenca/repository/RegistroExtraLiderRepository.java
@@ -1,4 +1,11 @@
 package com.example.controledepresenca.repository;
 
-public interface RegistroExtraLiderRepository {
+import com.example.controledepresenca.model.RegistroExtraLider;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RegistroExtraLiderRepository extends JpaRepository<RegistroExtraLider, Integer> {
+
+    RegistroExtraLider save(RegistroExtraLider  extra);
 }
