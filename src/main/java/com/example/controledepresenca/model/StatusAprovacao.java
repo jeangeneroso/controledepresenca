fa3174da505fc82b99f851a7c9ba78b01b37ca0f
Jean Generoso <jean.generoso@yahoo.com.br>
2026-08-13 18:38:37 -0300

    "fix: ajustes"

--- src/main/java/com/example/controledepresenca/model/StatusAprovacao.java
+++ src/main/java/com/example/controledepresenca/model/StatusAprovacao.java
@@ -2,21 +2,10 @@ package com.example.controledepresenca.model;
 
 import javax.persistence.*;
 
-@Entity
-@Table
+
 public enum StatusAprovacao {
     PENDENTE,
     APROVADO,
     REJEITADO;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    @Id
-    private Long id;
 }
