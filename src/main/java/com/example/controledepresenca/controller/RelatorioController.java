064a0ffd834d242494a5b1879bb3ecdcf00ed6c9
Jean Generoso <jean.generoso@yahoo.com.br>
2026-08-16 19:18:20 -0300

    "fix: Ajustes no RelatorioController.java"

--- src/main/java/com/example/controledepresenca/controller/RelatorioController.java
+++ src/main/java/com/example/controledepresenca/controller/RelatorioController.java
@@ -1,12 +1,11 @@
 package com.example.controledepresenca.controller;
 
+import com.example.controledepresenca.dto.RelatorioDTO;
+
 import com.example.controledepresenca.model.Relatorio;
 import com.example.controledepresenca.service.RelatorioService;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,14 +25,26 @@ public class RelatorioController {
     public List<Relatorio> getTodos() {
         return relatorioService.ListarTodos();
     }
-    
-    
-/*    @GetMapping("/relatorios")
-    public ResponseEntity<List<Relatorio>> buscarRelatorios(
-            @RequestParam(required = false) String nome,
-            @RequestParam(required = false) String cpf) {
-        return ResponseEntity.ok(relatorioService.buscarRelatorios(nome, cpf));
+
+    @GetMapping("/colaborador")
+    public List <Relatorio> getPorNomeColaborador(RelatorioDTO dto) {
+        return relatorioService.buscarPorNomeColaborador(dto);
     }
-*/
+
+    @GetMapping("/lider")
+    public List <Relatorio> getPorNomeLider(RelatorioDTO dto) {
+        return relatorioService.buscarPorNomeLider(dto);
+    }
+
+    @GetMapping("/cpf_colaborador")
+    public List <Relatorio> getPorCpfColaborador(RelatorioDTO dto) {
+        return relatorioService.buscarPorCpfColaborador(dto);
+    }
+
+    @GetMapping("/cpf_lider")
+    public List <Relatorio> getPorCpfLider(RelatorioDTO dto) {
+        return relatorioService.buscarPorCpfLider(dto);
+    }
+
        
 }
